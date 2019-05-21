db7a3ea2b5c010234f8e788bbb88a2e7a9b85896
Victor A. Hidrogo <VAHidrogo@muniservices.com>
2019-05-21 11:16:23 -0700

    Added queries to pull city and region data

--- annualgrowthchart.py
+++ annualgrowthchart.py
@@ -7,6 +7,7 @@ Created on May 21, 2019
 from tkinter import messagebox as msg
 import xlsxwriter
 
+import constants
 import utilities
 
 OUTPUT_NAME = 'Annualized Growth by Economic Category'
@@ -25,19 +26,26 @@ class AnnualizedGrowthChart:
         self.controller = controller
         self.selections = selections
         
-        self.query = ''
+        self.jurisdiction_query = ''
+        self.region_query = ''
         self.output_path = ''
         
         self.output_saved = False
+        
+        self.year_strings = []
         
         
     def main(self, jurisdiction):
         if self.selections.years >= MIN_YEARS:
             self.jurisdiction = jurisdiction
             
+            self._set_year_strings()
+            self._set_jurisdiction_query()
+            self._set_region_query()
+            print('jurisdiction_query', self.jurisdiction_query)
             self._set_output_path()
             self._output()
-            
+            print('region query', self.region_query)
             if self.output_saved and self.selections.open_output:
                 utilities.open_file(self.output_path)
                 
@@ -48,8 +56,48 @@ class AnnualizedGrowthChart:
                 )
             
             
-    def _set_query(self):
-        pass
+    def _set_year_strings(self):
+        period_count = (self.selections.years * 4)
+        
+        period_headers = utilities.get_period_headers(
+            count=period_count,
+            selections=self.selections,
+            prefix=constants.QUARTER_COLUMN_PREFIX
+            )
+        
+        # tuple with list of periods making up oldest and newest years
+        year_periods = (period_headers[:4], period_headers[-4:])
+        
+        self.year_strings = [
+            '+'.join([f'SUM({period})' for period in periods])
+            for periods in year_periods
+            ]
+            
+            
+    def _set_jurisdiction_query(self):
+        self.jurisdiction_query = f'''
+            SELECT {constants.CATEGORY_ID_COLUMN_NAME},
+                {self.year_strings[0]},{self.year_strings[-1]}
+            
+            FROM {constants.CATEGORY_TOTALS_TABLE}
+             
+            WHERE {constants.TAC_COLUMN_NAME}={self.jurisdiction.tac}
+            
+            ORDER BY {constants.CATEGORY_ID_COLUMN_NAME}
+            '''
+        
+        
+    def _set_region_query(self):
+        self.region_query = f'''
+            SELECT {constants.CATEGORY_ID_COLUMN_NAME},
+                {self.year_strings[0]},{self.year_strings[-1]}
+            
+            FROM {constants.CATEGORY_TOTALS_TABLE}{constants.REGION_SUFFIX}
+             
+            WHERE {constants.REGION_ID_COLUMN_NAME}={self.jurisdiction.region_id}
+            
+            ORDER BY {constants.CATEGORY_ID_COLUMN_NAME}
+            '''
         
         
     def _set_output_path(self):
