16ad6b3bf432eaefd82c9d44096bf181589b0342
Victor A. Hidrogo <VAHidrogo@muniservices.com>
2019-05-29 12:43:51 -0700

    Add tests for next_period() function

--- tests.py
+++ tests.py
@@ -36,6 +36,34 @@ class StarsAppTests(unittest.TestCase):
         self.assertEqual(
             utilities.format_permit_number('123456789'), '123-456789'
             )
+        
+        
+    def test_next_older_period_quarter_one(self):
+        # is the next older period returned when the quarter is 1?
+        self.assertEqual(
+            utilities.next_period((2019, 1), newer=False), (2018, 4)
+            )
+        
+        
+    def test_next_older_period(self):
+        # is the next older period returned?
+        self.assertEqual(
+            utilities.next_period((2019, 2), newer=False), (2019, 1)
+            )
+        
+    
+    def test_next_newer_period_quarter_four(self):
+        # is the next newer period returned when the quarter is 1?
+        self.assertEqual(
+            utilities.next_period((2018, 4), newer=True), (2019, 1)
+            )
+        
+        
+    def test_next_newer_period(self):
+        # is the next newer period returned?
+        self.assertEqual(
+            utilities.next_period((2019, 1), newer=True), (2019, 2)
+            )
 
 
 if __name__ == '__main__':
